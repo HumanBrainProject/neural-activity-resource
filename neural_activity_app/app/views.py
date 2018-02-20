25473a18406b05bb251655f91644c073349df920
Onur Ates <onur.ates@unic.cnrs-gif.fr>
2018-02-20 17:01:38 +0100

    add neural activity app

---
+++ neural_activity_app/app/views.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+from django.http import HttpResponse
+from neo import io
+# import numpy as np
+# from django.core.serializers.json import DjangoJSONEncoder
+# from neo.io import get_io
+import jsonpickle
+import jsonpickle.ext.numpy as jsonpickle_numpy
+from datetime import datetime
+
+
+jsonpickle_numpy.register_handlers()
+
+
+class DatetimeHandler(jsonpickle.handlers.BaseHandler):
+    def flatten(self, obj, data):
+        return obj.strftime('%Y-%m-%d %H:%M:%S.%f')
+
+
+jsonpickle.handlers.registry.register(datetime, DatetimeHandler)
+
+
+def browse(request):
+    """
+    display block
+    """
+
+    # read neo file from hd
+    r = io.AlphaOmegaIO(filename='File_AlphaOmega_2.map')
+    block = r.read_block(lazy=False, cascade=True)
+
+    block_data = {
+        # 'annotations': block.annotations,
+        # 'channel_indexes': block.channel_indexes,
+        'description': block.description,
+        # 'file_datetime': block.file_datetime,
+        'file_origin': block.file_origin,
+        # 'index': block.index,
+        'name': block.name,
+        'rec_datetime': block.rec_datetime,
+        'segments': [
+            {
+                'name': s.name,
+                # 'annotations': s.annotations,
+                'description': s.description,
+                # 'epochs': s.epochs,
+                # 'events': s.events,
+                'spiketrains': s.spiketrains,
+                'rec_datetime': s.rec_datetime,
+                # 'irregularlysampledsignals': s.irregularlysampledsignals,
+                # 'index': s.index,
+                # 'file_origin': s.file_origin,
+                # 'block': s.block,
+                'analogsignals': s.analogsignals,
+            }
+            for s in block.segments],
+    }
+
+    # encoder = DjangoJSONEncoder(ensure_ascii=False, indent=4)
+    # return render(request, 'browse.html', {'data': encoder.encode(block_data)})
+    return render(request, 'browse.html', {'data': jsonpickle.encode(block_data)})
+
+
+def segment(request):
+    """
+    display chosen segment details
+    """
+    return HttpResponse("test")
+
+
+def home(request):
+    """
+    home page
+    """
+    return HttpResponse("test")
+
+
---
+++ neural_activity_app/neural_activity_app/urls.py
@@ -0,0 +1,31 @@
+"""neural_activity_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from app.views import browse, segment, home
+# from rest_framework import routers
+
+# router = routers.DefaultRouter()
+# router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    # url(r'^', include(router.urls)),
+    url(r'^admin/', admin.site.urls),
+    url(r'^browse/', browse, name='browse'),
+    url(r'^segment/', segment, name='segment'),
+    url(r'^home/', home, name='home'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
