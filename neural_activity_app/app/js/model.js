1b94a32fc9b4b8a6f18fe87b9671714de1c11339
HFragnaud <helissande.fragnaud@gmail.com>
2018-11-21 16:38:16 +0100

    change angularjs request as external to the app.

--- neural_activity_app/app/js/model.js
+++ neural_activity_app/app/js/model.js
@@ -1,50 +1,43 @@
-var ApiCommunicationServices = angular.module('ApiCommunicationServices', ['ngResource', 'ngCookies', 'btorfs.multiselect']);
+var ApiCommunicationServices = angular.module('ApiCommunicationServices', ['ngResource', 'ngCookies']);
 
-// ApiCommunicationServices.factory('ScientificModelRest', ['$resource',
+ApiCommunicationServices.value('baseURL', 'https://localhost:8000/');
+
+ApiCommunicationServices.factory('BlockDataRest', ['$resource', 'baseURL',
+    function($resource, baseURL) {
+        return $resource(baseURL + 'blockdata/', { id: '@eUuid' }, {
+            get: { method: 'GET', params: { format: 'json' }, isArray: false },
+        })
+    }
+]);
+
+ApiCommunicationServices.factory('SegmentDataRest', ['$resource', 'baseURL',
+    function($resource, baseURL) {
+        return $resource(baseURL + 'segmentdata/', { id: '@eUuid' }, {
+            get: { method: 'GET', params: { format: 'json' }, isArray: false },
+        })
+    }
+]);
+
+ApiCommunicationServices.factory('AnalogSignalDataRest', ['$resource', 'baseURL',
+    function($resource, baseURL) {
+        return $resource(baseURL + 'analogsignaldata/', { id: '@eUuid' }, {
+            get: { method: 'GET', params: { format: 'json' }, isArray: false },
+        })
+    }
+]);
+
+// ApiCommunicationServices.factory('CollabIDRest', ['$resource',
 //     function($resource) {
-//         return $resource('models/:uuid', { id: '@eUuid' }, {
-//             get: { method: 'GET', params: { format: 'json', app_id: 'app_id', web_app: 'True' }, isArray: false },
-//             post: { method: 'POST', params: { format: 'json', app_id: 'app_id', web_app: 'True' }, headers: { 'Content-Type': 'application/json' } },
-//             put: { method: 'PUT', params: { format: 'json', app_id: 'app_id', web_app: 'True' }, headers: { 'Content-Type': 'application/json' } }
-//         });
+//         return $resource('collabid/:uuid', { id: '@eUuid' }, {
+//             get: { method: 'GET', params: { format: 'json' }, isArray: false },
+//         })
 //     }
 // ]);
-ApiCommunicationServices.factory('BlockDataRest', ['$resource',
-    function($resource) {
-        return $resource('blockdata/', { id: '@eUuid' }, {
-            get: { method: 'GET', params: { format: 'json' }, isArray: false },
-        })
-    }
-]);
 
-ApiCommunicationServices.factory('SegmentDataRest', ['$resource',
-    function($resource) {
-        return $resource('segmentdata/', { id: '@eUuid' }, {
-            get: { method: 'GET', params: { format: 'json' }, isArray: false },
-        })
-    }
-]);
-
-ApiCommunicationServices.factory('AnalogSignalDataRest', ['$resource',
-    function($resource) {
-        return $resource('analogsignaldata/', { id: '@eUuid' }, {
-            get: { method: 'GET', params: { format: 'json' }, isArray: false },
-        })
-    }
-]);
-
-ApiCommunicationServices.factory('CollabIDRest', ['$resource',
-    function($resource) {
-        return $resource('collabid/:uuid', { id: '@eUuid' }, {
-            get: { method: 'GET', params: { format: 'json' }, isArray: false },
-        })
-    }
-]);
-
-ApiCommunicationServices.factory('AppIDRest', ['$resource',
-    function($resource) {
-        return $resource('appid/:uuid', { id: '@eUuid' }, {
-            get: { method: 'GET', params: { format: 'json' }, isArray: false },
-        })
-    }
-]);
+// ApiCommunicationServices.factory('AppIDRest', ['$resource',
+//     function($resource) {
+//         return $resource('appid/:uuid', { id: '@eUuid' }, {
+//             get: { method: 'GET', params: { format: 'json' }, isArray: false },
+//         })
+//     }
+// ]);
