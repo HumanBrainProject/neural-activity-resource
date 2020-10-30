42433fe7d7ba8012e61c8e9110b8f4350b54fcf3
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2020-10-30 15:32:59 +0100

    Basic framework for NAR app v2, based on create-react-app

---
+++ apps/nar-v2/src/App.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://neuropsi.cnrs.fr/">
+        CNRS
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [
+  {"label": "Patch clamp recording"},
+  {"label": "Intracellular sharp-electrode recording"},
+  {"label": "ECoG"},
+  {"label": "EEG"},
+  {"label": "Multi-electrode array recording"},
+  {"label": "Two-photon calcium imaging"},
+  {"label": "fMRI"},
+  {"label": "All datasets"},
+];
+
+export default function App(props) {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <MenuIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Neural Activity Resource (alpha)
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container className={classes.cardGrid} maxWidth="lg">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card.label} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.label}
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
---
+++ apps/nar-v2/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import initAuth from './auth';
+import App from './App';
+
+
+function renderApp(auth) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App auth={auth} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
+
+window.addEventListener('DOMContentLoaded', () => initAuth(renderApp));
